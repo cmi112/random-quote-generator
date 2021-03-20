e7128b42a622a76c0d7fe6f69b5db582ab6ca022
cmi112 <cabdiqanimuuse7@gmail.com>
2021-03-20 08:34:34 +0100

    initail commit

---
+++ script.js
@@ -0,0 +1,17 @@
+import { quotes } from "./quotes.js";
+let btn = document.querySelector("button");
+btn = document.querySelector("button").addEventListener("click", () => {
+  let p = document.querySelector("p");
+  let randomQuote = Math.floor(Math.random() * quotes.length);
+  function colorGenerator(transparency = 0.3) {
+    let randomColor = () => Math.round(Math.random() * 255);
+    return `rgb(${randomColor()},${randomColor()},${randomColor()}, ${transparency})`;
+  }
+  for (let i = 0; i < quotes.length; i++) {
+    p.innerText = ` ${quotes[randomQuote].text},  "${quotes[randomQuote].author}"  `;
+  }
+  let color = (document.querySelector(
+    ".quote-par"
+  ).style.backgroundColor = colorGenerator());
+  //   console.log(color);
+});
